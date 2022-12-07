7d81cf0660bc559d4ce8c3eb7d74eff4e6d86b8a
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2022-12-07 23:11:03 +0530

    Add files via upload

---
+++ area of square.c
@@ -0,0 +1,14 @@
+/* Program to find area of a square using single variable */
+
+#include <stdio.h>
+
+//using single variable
+
+int main(void) {
+  //area of a square
+ int side;
+  printf("eneter the value of side of a square =");
+  scanf("%d",&side);
+  printf("the area of a square is %d",side*side);
+  return 0;
+  }
