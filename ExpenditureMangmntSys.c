5cead737414262cffebba7d14cb3194b94ddeb8c
darshan45672 <drshnbhandary@gmail.com>
2022-12-11 21:08:40 +0530

    login page prototype

--- ExpenditureMangmntSys.c
+++ ExpenditureMangmntSys.c
@@ -4,7 +4,6 @@
 #include<windows.h>
 //decalre ypur functions here
 void login();
-int check(char a[], char b[]);
 
 //main function
 int main(void) {
@@ -16,22 +15,24 @@ int main(void) {
 //define your functions here
 void login(){
      system("cls");
-     char UserPsswrd[10];
-     char UserNameInpt[10];
+     char UserPsswrd[10], UserNameInpt[10];
      printf("\n\n\n\n\n\n##########################################################################################################################################################################");
      printf("\n\n\n\t\t\t\t\t\t\t\t\t EXPENDITURE MANAGEMENT SYSTEM");
      printf("\n\n\n\t\t\t\t\t\t\t\t\t USERNAME : \t");
      scanf("%s",UserNameInpt);
      printf("\n\n\t\t\t\t\t\t\t\t\t PASSWORD : \t");
      scanf("%s",&UserPsswrd);
-     if(check(UserNameInpt, UserPsswrd)){
-          printf("hi");
+     if(strcmp(UserNameInpt, "Admin")==0){
+          if (strcmp(UserPsswrd, "13579")==0)
+          {
+               printf("\n\n\n\t\t\t\t\t\t\t\t\tLogin Sucessfull !!\n");
+               printf("\n\t\t\t\t\t\t\t\t\tWelcome %s \n",UserNameInpt);
+          }
+          else{
+               printf("\n\n\n\t\t\t\t\t\t\t\t\tInvalid Password\n");
+          }
      }
-
-}
-
-int check(char a[], char b[]){
-     char User[] = "Admin";
-     char Pass[] = "13579";
-     return(strcmp(User, a) && strcmp(Pass, b));
+     else{
+          printf("\n\n\n\t\t\t\t\t\t\t\t\tInvalid Username\n");
+     }
 }
