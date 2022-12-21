f179bc98fe78de90bbd0dfaabb8a6ddec68c5d4a
srakshitha296 <srakshitha296@gmail.com>
2022-12-21 15:45:21 +0530

    trial

--- srakshitha296/login.c
+++ srakshitha296/login.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include<string.h>
 
+void registration();
+void entry();
 void guide();
 void login();
 void guide();
+
+struct details {
+char  userName[20];
+char  passWord[20];
+};
 
 
 // Main function
@@ -12,54 +19,45 @@ int main() {
 	printf("                                                                                ________________________________________________\n");
     printf("                                                                               |           EXPENDITURE MANAGEMENT SYSTEM        |\n");
     printf("                                                                               |________________________________________________|\n\n");
-    guide();
+    entry();
 int choice;
 	printf(" \n\n                                                                              Enter your choice here : ");
 	scanf("%d",&choice);
-
 	switch(choice) {
 		/*case 1 : {
-			void register();
+			 registration();
 		     break;
 			 } */
 
 		case 2 :{
 		          login();
 		         break;	 
-		        }	 
+		        }
+	}	 
 
-	    /*case 3 :{
-		          update();
-				 break;
-				 }
-
-	     case 4 :{
-			      status();
-				 break;
-				 } 
-
-	    case 5 : {
-			       guide();
-				  break;
-				  } */
-				  default : printf("invalid choice");
-	}
+	
      	
 	 	return 0;
 }
 
 
+void entry() {
+printf("\n\n");
+printf("                                                                                      1.    REGISTER\n");
+printf("                                                                                      2.    LOGIN\n");
+}
 
-
+void registration() {
+	
+	
+	}
 
 // This function represents various operations performed in this program.
 void guide() {
 printf("\n\n");
-printf("                                                                                      1.    REGISTER\n");
-printf("                                                                                      2.    LOGIN\n");
-printf("                                                                                      3.    UPDATE\n");	
-printf("                                                                                      4.    STATUS\n");	
-//printf("                                                                                      5.    HOME PAGE\n");	
+printf("                                                                                      1.    UPDATE\n");	
+printf("                                                                                      2.    STATUS\n");	
+//printf("                                                                                     3.    HOME PAGE\n");	
 }
 
 
@@ -88,6 +86,29 @@ void login() {
 	printf("                                                                                ________________________________________________\n");
     printf("                                                                               |    WELCOME TO EXPENDITURE MANAGEMENT SYSTEM    |\n");
     printf("                                                                               |________________________________________________|\n\n");
+	 guide();
+	int choice;
+	printf(" \n\n                                                                              Enter your choice here : ");
+	scanf("%d",&choice);
+	switch(choice) {
+
+	    /*case 1 :{
+		          update();
+				 break;
+				 }
+
+	     case 2 :{
+			      status();
+				 break;
+				 } 
+
+	    case 3 : {
+			       guide();
+				  break;
+				  } */
+				  default : printf("invalid choice");
+	}
+	
      			exit(0);
      			} else {
                 printf(" \n                                                                                        INVALID PASSWORD ! TRY AGAIN\n\n");
@@ -98,9 +119,10 @@ void login() {
      	 printf("\n                                                                                        INVALID USERNAME ! TRY AGAIN\n\n");
      	 chance--;
         }
-     	 if(chance == 0)
+     	 if(chance == 0){
 		 printf("\n\n                                                                                                   ACCESS DENIED");
-     		
+     		exit(0); 
+		 }
 		 }while(chance != 0);
-
+       
 }
